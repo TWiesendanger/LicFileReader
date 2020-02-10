054349e97ac075cf6cb9951c54406c4f2ae26aa6
TWiesendanger <tobias.wiesendanger@gmail.com>
2020-02-10 23:48:14 +0100

    Added AddChild Function for Grid

--- ReadLicense.ps1
+++ ReadLicense.ps1
@@ -1,12 +1,3 @@
-#TODO
-#Paste Text from Clipboard
-#Multiple Files
-
-#Next Step
-#Fill Grid with Product Information from Grid
-
-
-
 Clear-Host
 #Initialize
 [System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms') | out-null
@@ -18,8 +9,6 @@ Clear-Host
 [System.Reflection.Assembly]::LoadWithPartialName('PresentationCore') | out-null
 [System.Reflection.Assembly]::LoadFrom('res\assembly\MahApps.Metro.dll') | out-null
 [System.Reflection.Assembly]::LoadFrom('res\assembly\System.Windows.Interactivity.dll') | out-null
-
-
 
 # When compiled with PS2EXE the variable MyCommand contains no path anymore
 
@@ -39,33 +28,123 @@ $MainformIcon = $Path + "\res\mum.png"
 ##############################################################
 #                Functions                       #
 ##############################################################
+Function Colorize {
+    foreach ($item in $WPFDatagridPack.Items) {
+
+        #if ($item.cells[0].value -eq $null) { }
+        #else {
+        #   $row.Cells[0].Style.BackColor = 'Red'
+        #}     
+    }
+}
+Function CheckPackage {
+
+    #===========================================================================
+    # Clean Up
+    #===========================================================================
+    $IncrementParts = $IncrementArray.Split(" ")
+
+    for ($t = 0; $t -lt $IncrementParts.length; $t++) {
+        $IncrementParts[$t] = $IncrementParts[$t] -replace '[\\]+', '' -replace '"', "" -replace '\s', ""   #remove backslash ; remove apostrophe; remove all whitespace
+    }
+
+    #===========================================================================
+    # Search for Package
+    #===========================================================================
+    $searchVar = ("Package " + $IncrementParts[1] + "*") #Package + Featurecode from Increment
+
+    foreach ($line in [System.IO.File]::ReadLines($LicenseFile)) { 
+        If ($line | Where-Object { $_ -like $searchVar }) {
+            if ($line | Where-Object { $_ -like "*SIGN*" }) {
+                $searchVar = "Stop"
+            }
+            else {
+                    
+                $PackageArray += $line
+                $searchVar = "*"
+            }
+        }
+    }
 
 
-Function CreateIncrement {
+    #===========================================================================
+    # Package
+    #===========================================================================
+    If ($PackageArray -eq $null) {
+        #No Package found / Must be Single Product
 
-    #Line 1
-    $lineNo = 1
-    $IncrementLine = ($IncrementArray -split '\r?\n')[$lineNo - 1]  # -> 'line 1 Increment
+        #Seat Count
+        $varSeat = $IncrementParts[5]
+        #Product Name (Feature)
+        $varFeature = $FeatureHashtable.($IncrementParts[1])
+        #FeatureCode
+        $varFeatureCode = $IncrementParts[1]
+        #SerialNumber
+        $varSerialNumber = $IncrementParts | Where-Object { $_ -like "*SN=*" }
+        $varSerialNumber = $varSerialNumber.SubString(3, $varSerialNumber.length - 3)
+        #Issue Date
+        $varIssueDate = $IncrementParts | Where-Object { $_ -like "*ISSUED=*" }
+        $varIssueDate = $varIssueDate.Substring(7, $varIssueDate.Length - 7)
+        #Expiration Date
+        $varExpiration = $IncrementParts | Select-String -pattern '^\d{2}[\-]\w{3}[\-]\d{4}'
+        If ($varExpiration -eq $null) {
+            $varExpiration = "Permanent" #If there is no regex pattern matching it goes to permanent
+        }
 
-    $IncrementLineParts = $IncrementLine.Split(" ")
+        #Create Custom Object
+        $SingleHeader = [pscustomobject]@{Seats = $varSeat; Feature = $varFeature; FeatureCode = $varFeatureCode; SerialNumber = $varSerialNumber; IssueDate = $varIssueDate; Expiration = $varExpiration }
+        $WPFDatagridInc.AddChild($SingleHeader)
+        #Log
+        #"Single Product" | Out-File 'Logfile.txt' -Append
+        #$SingleHeader | Out-File 'Logfile.txt' -Append
+    }
+    else {
+        #Build Header Custom Object
 
-    $Featurecode = $IncrementLineParts[1]
-    $ExpirationDate = $IncrementLineParts[4]
+        #Seat Count
+        $varSeat = $IncrementParts[5]
+        #Product Name (Feature)
+        $varFeature = $FeatureHashtable.($IncrementParts[1])
+        #FeatureCode
+        $varFeatureCode = $IncrementParts[1]
+        #SerialNumber
+        $varSerialNumber = $IncrementParts | Where-Object { $_ -like "*SN=*" }
+        $varSerialNumber = $varSerialNumber.SubString(3, $varSerialNumber.length - 3)
+        #Issue Date
+        $varIssueDate = $IncrementParts | Where-Object { $_ -like "*ISSUED=*" }
+        $varIssueDate = $varIssueDate.Substring(7, $varIssueDate.Length - 7)
+        #Expiration Date
+        $varExpiration = $IncrementParts | Select-String -pattern '^\d{2}[\-]\w{3}[\-]\d{4}'
 
-    #Line 4
-    $lineNo = 4
-    $IssuedLine = ($IncrementArray -split '\r?\n')[$lineNo - 1]  # -> 'line 4 Issued / SerialNr
+        #Create Custom Object
+        $PackageHeader = [pscustomobject]@{Seats = $varSeat; Feature = $varFeature; FeatureCode = $varFeatureCode; SerialNumber = $varSerialNumber; IssueDate = $varIssueDate; Expiration = $varExpiration }
+        $WPFDatagridPack.AddChild($PackageHeader)
+        #$WPFDatagridPack.Rows[0].Cells[0].Style.BackColor = 'Red'
+        #Log
+        #"Package" | Out-File 'Logfile.txt' -Append
+        #$PackageHeader | Out-File 'Logfile.txt' -Append
 
-    $IssuedLineParts = $IssuedLine.Split(" ")
 
-    $IssuedDate = $IssuedLineParts[0].Substring(8)
-    $SerialNr = $IssuedLineParts[2].Substring(3)
+        #===========================================================================
+        # Components
+        #===========================================================================
+        $Component = $PackageArray -match '\"(.*?)\"' #regex match between " to "
+        $ComponentParts = $Matches[0].Split(" ")
+    
+        for ($i = 0; $i -lt $ComponentParts.length; $i++) {
+        
+            $FeatureCodeCleaned = $ComponentParts[$i] -replace '[\\]+', '' -replace '"', "" -replace '\s', ""  #remove backslash ; remove apostrophe; remove all whitespace
+            $ProductLine = $FeatureHashtable.$FeatureCodeCleaned
 
+            $IncrementLine = [pscustomobject]@{Seats = " " ; Feature = $ProductLine; FeatureCode = $FeatureCodeCleaned } #Seat is set to " " so it triggers style template from xaml
+            $WPFDatagridPack.AddChild($IncrementLine)
+            #Log
+            #"Product" | Out-File 'Logfile.txt' -Append
+            #$IncrementLine | Out-File 'Logfile.txt' -Append    
+        }
+        #Write-Host $PackageArray
+    }
 
-    Write-Host "Feature Code: $Featurecode"  
-    Write-Host "Expiration Date: $ExpirationDate"  
-    Write-Host "Issue Date: $IssuedDate"  
-    Write-Host "Serial Number: $SerialNr"  
 
 }
 
@@ -74,9 +153,16 @@ Function ReadSource {
     $LicenseFile = $null
     $LicenseFile = $Files
 
-    # #Get FeatureCodeList
-    $content = ( Get-Content "H:\Dropbox (Data)\TWIWork\01_MuM\04_Programmierung\06_Powershell\Tools\LicFileReader\FeatureCodes.txt" | Out-String )
-    $Hashtable = ( Invoke-Expression $content )
+    #Log
+    #$logFile = "StartFile"
+    #$logFile | Out-File 'Logfile.txt'
+
+    $string = "*INCREMENT*"
+    $lineNumber = 0
+    $IncrementArray = $null #make sure it is empty
+
+    # Get FeatureCodeList
+    $FeatureHashtable = Get-Content -Path "H:\Dropbox (Data)\TWIWork\01_MuM\04_Programmierung\06_Powershell\Tools\LicFileReader\FeatureCodes.txt" | ConvertFrom-StringData
 
     #Define Server and Macadress
     $ServerLine = Get-Content $LicenseFile | Where-Object { $_ -like "*Server*" }
@@ -84,30 +170,28 @@ Function ReadSource {
     $global:ServerName = $ServerLineParts[1]
     $global:MACAdress = $ServerLineParts[2]
 
-
-    #Write-Host $IncrementBlock
-    $string = "*INCREMENT*"
-    $lineNumber = 0
-    $IncrementArray = $null #make sure it is empty
+    #===========================================================================
+    # Read Line after Line
+    #===========================================================================
 
     foreach ($line in [System.IO.File]::ReadLines($LicenseFile)) {   
-        $lineNumber++
+        #$lineNumber++
+    
         if ($line | Where-Object { $_ -like $string }) {
             Write-Host "Linenumber" + $lineNumber
-            if ($line | Where-Object { $_ -like "PACKAGE" }) {
-                CreateIncrement
+            if ($line | Where-Object { $_ -like "*SIGN*" }) {
+                $IncrementArray += $line #still add this last found line
+                CheckPackage
                 $string = "*INCREMENT*"
+                $IncrementArray = $null
             }
             else {
-                    
+                
                 $IncrementArray += $line
                 $string = "*"
-                $IncrementArray += " `n" #New Line
             }
         }        
     }
-
-    Write-Host $IncrementArray
 }
 
 Function Fillout {
@@ -165,7 +249,6 @@ $WPFSource.Add_PreviewDragOver( {
         [System.Object]$script:sender = $args[0]
         [System.Windows.DragEventArgs]$e = $args[1]
 
-
         $Script:Files = $e.Data.GetData([System.Windows.DataFormats]::FileDrop)
         If ($Files.Count -gt 1) {
             $e.Effects = [System.Windows.DragDropEffects]::None #Show Icon not allowed (Only one File)
@@ -184,8 +267,7 @@ $WPFSource.Add_PreviewDragOver( {
                 $e.Effects = [System.Windows.DragDropEffects]::None
                 $WPFTextBlockDrop.Text = "Wrong Filetype!"  
             }
-        }
-    
+        }  
         $e.Handled = $true
     })
 
@@ -198,7 +280,6 @@ $WPFSource.Add_Drop( {
         [System.Object]$script:sender = $args[0]
         [System.Windows.DragEventArgs]$e = $args[1]
     
-
         If ($e.Data.GetDataPresent([System.Windows.DataFormats]::FileDrop)) {
 
             $Script:Files = $e.Data.GetData([System.Windows.DataFormats]::FileDrop)
@@ -207,6 +288,7 @@ $WPFSource.Add_Drop( {
             $WPFTextBlockDrop.Text = $FileName
             ReadSource
             Fillout
+            Colorize
         }
     })
 
